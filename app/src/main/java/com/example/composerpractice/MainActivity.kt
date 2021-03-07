1bb1b552e9d24a3fb0a39f26b4a33180b0cc6da5
neesan <exp.ym.lvl@gmail.com>
2021-03-08 01:25:01 +0900

    レッスン２まで終わり　サンプル通りに書こうとするとAPI無くなってたりして詰まる

--- app/src/main/java/com/example/composerpractice/MainActivity.kt
+++ app/src/main/java/com/example/composerpractice/MainActivity.kt
@@ -3,34 +3,66 @@ package com.example.composerpractice
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composerpractice.ui.theme.ComposerPracticeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            NewsStory()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hallo $name")
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hallo $name")
+//}
 
 // 主な制限として、コンポーズ可能な関数にはパラメータを使用できません。
 // このため、Greeting() 関数を直接プレビューすることはできません。
 // 代わりに、PreviewGreeting() という 2 番目の関数を作成します。
 // この関数は、適切なパラメータで Greeting() を呼び出します。
 // @Composable の前に @Preview アノテーションを追加します。
+//@Preview
+//@Composable
+//fun PreviewGreeting() {
+//    Greeting("Android")
+//}
+
+@Composable
+fun NewsStory() {
+    val image = painterResource(R.drawable.header)
+
+    Column(modifier = Modifier.padding(16.dp))
+    {
+        val imageModifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth()
+
+        Image(image,
+            null,
+            modifier = imageModifier,
+            contentScale = ContentScale.Crop)
+
+        Spacer(Modifier.height(16.dp))
+
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
+}
+
 @Preview
 @Composable
-fun PreviewGreeting(){
-    Greeting("Android")
+fun DefaultPreview() {
+    NewsStory()
 }
