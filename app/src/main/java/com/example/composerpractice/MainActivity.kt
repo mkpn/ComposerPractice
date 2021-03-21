fff0145d2e472e117c05f05c2381c31132634e2c
neesan <exp.ym.lvl@gmail.com>
2021-03-22 02:01:18 +0900

    レッスン２まで終わり　サンプル通りに書こうとするとAPI無くなってたりして詰まる

--- app/src/main/java/com/example/composerpractice/MainActivity.kt
+++ app/src/main/java/com/example/composerpractice/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -41,23 +45,40 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun NewsStory() {
     val image = painterResource(R.drawable.header)
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(modifier = Modifier.padding(16.dp))
+        {
+            val imageModifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp))
 
-    Column(modifier = Modifier.padding(16.dp))
-    {
-        val imageModifier = Modifier
-            .height(180.dp)
-            .fillMaxWidth()
+            Image(
+                image,
+                null,
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
+            )
 
-        Image(image,
-            null,
-            modifier = imageModifier,
-            contentScale = ContentScale.Crop)
+            Spacer(Modifier.height(16.dp))
 
-        Spacer(Modifier.height(16.dp))
-
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+            Text(
+                "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis)
+            Text(
+                "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                "December 2018",
+                style = typography.body2
+            )
+        }
     }
 }
 
